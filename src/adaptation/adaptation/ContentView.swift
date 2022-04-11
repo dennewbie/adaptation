8963caf0f18e88cd2df7dee1b574650817e44e7a
Denny Caruso <caruso.denny@gmail.com>
2022-04-11 09:03:40 +0200

    added src + design

---
+++ src/adaptation/adaptation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  adaptation
+//
+//  Created by Denny Caruso on 11/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/adaptation/adaptation/adaptationApp.swift
@@ -0,0 +1,17 @@
+//
+//  adaptationApp.swift
+//  adaptation
+//
+//  Created by Denny Caruso on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct adaptationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
