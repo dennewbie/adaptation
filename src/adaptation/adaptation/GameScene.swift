cfef6a495cb5de2484ced6186c55042877f83fed
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-04-11 19:09:43 +0200

    Screen view
    try screen vir

--- src/adaptation/adaptation/GameScene.swift
+++ src/adaptation/adaptation/GameScene.swift
@@ -9,18 +9,17 @@ import SpriteKit
 
 class GameScene: SKScene {
     var Start = SKSpriteNode()
-    var End = SKSpriteNode()
     var User = SKSpriteNode()
     var cam = SKCameraNode()
-    var maze = SKSpriteNode()
+    var Disp = SKScene()
     
     let Speed: CGFloat = 2.0
     
     override func didMove(to view: SKView) {
         //Init
         Start = self.childNode(withName: "Start") as! SKSpriteNode
-        End = self.childNode(withName: "End") as! SKSpriteNode
-        maze = self.childNode(withName: "Maze/Floor") as! SKSpriteNode
+        Disp = SKScene(size:CGSize(width: 646.487, height: 1090.27))
+        Disp.position = CGPoint(x: -213.856, y: -707.118)
         
         //User Init
         UserInit()
@@ -29,15 +28,18 @@ class GameScene: SKScene {
         cam.xScale = 0.5
         cam.yScale = 0.5
         User.addChild(cam)
-        scene?.camera = cam
+        Disp.camera = cam
+        
+        scene?.addChild(Disp)
         
         //Swipe Init
-        SwipeInit(view: view)
+//        SwipeInit(view: view)
     }
     
     func UserInit() {
         //User Create
-        User = SKSpriteNode(color: SKColor.cyan, size: CGSize(width: 50, height: 53))
+        User = SKSpriteNode(imageNamed: "maleCharacter")
+        User.size = CGSize(width: 50, height: 53)
         User.position = Start.position
         scene?.addChild(User)
         
@@ -49,45 +51,46 @@ class GameScene: SKScene {
         
     }
     
-    func SwipeInit(view: SKView) {
-        //Swipe
-        let swiperRight = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeRight(sender:)))
-        swiperRight.direction = .right
-        view.addGestureRecognizer(swiperRight)
-        
-        let swiperDown = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeDown(sender:)))
-        swiperDown.direction = .down
-        view.addGestureRecognizer(swiperDown)
-        
-        let swiperUp = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeUp(sender:)))
-        swiperUp.direction = .up
-        view.addGestureRecognizer(swiperUp)
-        
-        let swiperLeft = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeLeft(sender:)))
-        swiperLeft.direction = .left
-        view.addGestureRecognizer(swiperLeft)
-        
-    }
-
-    
-    @objc func swipeRight(sender: UISwipeGestureRecognizer){
-        debugPrint("Swipe Right");
-        User.run(SKAction.moveBy(x: maze.size.width , y:0, duration: Speed))
-    }
-    
-    @objc func swipeDown (sender: UISwipeGestureRecognizer){
-        debugPrint("Swipe Dowm")
-        User.run(SKAction.moveBy(x: 0, y: -maze.size.height, duration: Speed))
-    }
-    
-    @objc func swipeUp (sender: UISwipeGestureRecognizer){
-        debugPrint("Swipe Up")
-        User.run(SKAction.moveBy(x: 0, y: maze.size.height, duration: Speed))
-    }
-    
-    @objc func swipeLeft (sender: UISwipeGestureRecognizer){
-        debugPrint("Swipe Left")
-        User.run(SKAction.moveBy(x: -maze.size.width , y: 0, duration: Speed))
-    }
-    
+//    func SwipeInit(view: SKView) {
+//        //Swipe
+//        let swiperRight = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeRight(sender:)))
+//        swiperRight.direction = .right
+//        view.addGestureRecognizer(swiperRight)
+//
+//        let swiperDown = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeDown(sender:)))
+//        swiperDown.direction = .down
+//        view.addGestureRecognizer(swiperDown)
+//
+//        let swiperUp = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeUp(sender:)))
+//        swiperUp.direction = .up
+//        view.addGestureRecognizer(swiperUp)
+//
+//        let swiperLeft = UISwipeGestureRecognizer(target: self, action: #selector(GameScene.swipeLeft(sender:)))
+//        swiperLeft.direction = .left
+//        view.addGestureRecognizer(swiperLeft)
+//
+//    }
+//
+//
+//    @objc func swipeRight(sender: UISwipeGestureRecognizer){
+//        debugPrint("Swipe Right");
+//        User.run(SKAction.moveBy(x: maze.size.width , y:0, duration: Speed))
+//    }
+//
+//    @objc func swipeDown (sender: UISwipeGestureRecognizer){
+//        debugPrint("Swipe Dowm")
+//        User.run(SKAction.moveBy(x: 0, y: -maze.size.height, duration: Speed))
+//    }
+//
+//    @objc func swipeUp (sender: UISwipeGestureRecognizer){
+//        debugPrint("Swipe Up")
+//        User.run(SKAction.moveBy(x: 0, y: maze.size.height, duration: Speed))
+//    }
+//
+//    @objc func swipeLeft (sender: UISwipeGestureRecognizer){
+//        debugPrint("Swipe Left")
+//
+//        User.run(SKAction.moveBy(x: -maze.size.width , y: 0, duration: Speed))
+//    }
+//
 }
