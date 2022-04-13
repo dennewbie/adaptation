d1557a22106052f3223b07ea65579c2581d23034
Denny Caruso <caruso.denny@gmail.com>
2022-04-13 18:20:06 +0200

    fixed levels view

--- src/adaptation/adaptation/LevelsView.swift
+++ src/adaptation/adaptation/LevelsView.swift
@@ -8,25 +8,50 @@
 import SwiftUI
 
 struct LevelsView: View {
-    var body: some View {
-        VStack {
-            HStack {
-                BackButtonView()
+    @State private var selectedButton: Int? = nil
     
-                Text("Levels").foregroundColor(Color(red: 37/255, green: 17/255, blue: 97/255)).fontWeight(.bold).font(.system(size: 40)).padding(.trailing, 66)
-            }.frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight / 10, alignment: .center)
-            
-            HStack {
-                SingleLevelsView(level: "1")
-                SingleLevelsView(level: "2")
+    init() {
+        UINavigationBar.setAnimationsEnabled(false)
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    VStack {
+                        NavigationLink(destination: HomeView(), tag: 1, selection: $selectedButton) {
+                            CustomButton(buttonAction: {
+                                print("Back Button clicked")
+                                self.selectedButton = 1
+                            }, imageName: "arrowshape.turn.up.backward.circle", buttonHeight: 51, buttonWidth: 51, buttonAlignment: .topLeading, buttonColor: UIColor(red: 132 / 255, green: 93 / 255, blue: 250 / 255, alpha: 1.0), systemImage: true)
+                        }
+                    }
+
+                    VStack {
+                        Text("Levels")
+                            .foregroundColor(Color(red: 37/255, green: 17/255, blue: 97/255))
+                            .fontWeight(.bold)
+                            .font(.system(size: 40))
+                    }
+                }
+                .padding(.top, 0)
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight / 5, alignment: .topLeading)
+                
+                VStack {
+                    HStack {
+                        SingleLevelsView(level: "1")
+                        SingleLevelsView(level: "2")
+                    }
+                    
+                    HStack {
+                        SingleLevelsView(level: "3")
+                        SingleLevelsView(level: "4")
+                    }
+                }
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight / 1.5, alignment: .top)
             }
-            
-            HStack {
-                SingleLevelsView(level: "3")
-                SingleLevelsView(level: "4")
-            }
-            
-            Spacer()
         }
+        .navigationBarTitle("Title")
+        .navigationBarHidden(true)
     }
 }
--- src/adaptation/adaptation/SingleLevelsView.swift
+++ src/adaptation/adaptation/SingleLevelsView.swift
@@ -11,23 +11,30 @@ struct SingleLevelsView: View {
     let level : String
     let colorDark : Color = Color(red: 37 / 255, green: 17 / 255, blue: 97 / 255)
     let colorLight : Color = Color(red: 132 / 255, green: 93 / 255, blue: 250 / 255)
-    var levelcomplete : Bool = false
+    var levelComplete : Bool = false
    
     var body: some View {
-        if levelcomplete {
+        if (levelComplete) {
             ZStack {
-                Rectangle().frame(width: 170, height: 138).foregroundColor(colorLight).cornerRadius(30)
-                
-                Text(level).foregroundColor(.white)
+                Rectangle()
+                    .frame(width: UIScreen.screenWidth / 2.5, height: UIScreen.screenHeight / 6)
+                    .foregroundColor(colorLight)
+                    .cornerRadius(30)
+                Text(level)
+                    .foregroundColor(.white)
             }
         } else {
             ZStack{
-                Rectangle().frame(width: 170, height: 138).foregroundColor(colorDark).cornerRadius(30).padding(8)
-                    
-                Text(level).foregroundColor(.white).font(.system(size: 35)).fontWeight(.bold)
+                Rectangle()
+                    .frame(width: UIScreen.screenWidth / 2.5, height: UIScreen.screenHeight / 6)
+                    .foregroundColor(colorDark)
+                    .cornerRadius(30)
+                    .padding(8)
+                Text(level)
+                    .foregroundColor(.white)
+                    .font(.system(size: 35))
+                    .fontWeight(.bold)
             }
         }
     }
 }
-
-
