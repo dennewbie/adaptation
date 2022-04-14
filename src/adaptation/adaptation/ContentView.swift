9186a94ab9847dfaf19192e13ac669664950636b
Denny Caruso <caruso.denny@gmail.com>
2022-04-14 17:52:27 +0200

    added pause Button in ContentView Maze

--- src/adaptation/adaptation/ContentView.swift
+++ src/adaptation/adaptation/ContentView.swift
@@ -23,34 +23,56 @@ func playSound(sound: String, type: String) {
 }
 
 struct ContentView: View {
+    let contentViewColor: UIColor = UIColor(red: 132 / 255, green: 93 / 255, blue: 250 / 255, alpha: 1.0)
+    @State private var selectedButton: Int? = nil
     init() {
         UINavigationBar.setAnimationsEnabled(false)
     }
     
     var scene: SKScene {
         let scene = SKScene(fileNamed: "GameScene")
-        scene!.size = CGSize(width: 820, height: 1670)
+        scene!.size = CGSize(width: 820, height: 1570)
         scene?.scaleMode = .aspectFit
         return scene!
     }
     
     
     var body: some View {
-        VStack {
-            NavigationView {
-                SpriteView(scene: scene)
-                    .edgesIgnoringSafeArea(.all)
+        NavigationView {
+            VStack {
+                VStack {
+                    NavigationLink(destination: PauseView(), tag: 2, selection: $selectedButton) {
+                        CustomButton(buttonAction: {
+                            print("pause button clicked")
+                            self.selectedButton = 2
+                        }, imageName: "pause.circle", buttonHeight: 60, buttonWidth: 60, buttonAlignment: .topTrailing, buttonColor: contentViewColor, systemImage: true)
+                    }
+                }
+                .frame(width: UIScreen.screenWidth, height: 60, alignment: .topTrailing)
+                
+                
+                VStack {
+                    SpriteView(scene: scene)
+                        .edgesIgnoringSafeArea(.all)
+                }
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight - 230, alignment: .center)
+                
+                VStack {
+                    LabelView()
+                }
+                .frame(width: UIScreen.screenWidth, height: 50, alignment: .bottom)
+                .padding(.bottom, 20)
             }
-            .navigationBarTitle("Title")
-            .navigationBarHidden(true)
-            
-            LabelView()
-        }
-        .onAppear(perform: {
+            .onAppear(perform: {
                 playSound(sound: "musicGame", type: "wav")
                 audioPlayer?.numberOfLoops = 100
                 audioPlayer?.volume = 0.5
-        })
+            })
+        }
+        .navigationBarTitle("Title")
+        .navigationBarHidden(true)
+        
     }
 }
 
@@ -65,7 +87,7 @@ struct LabelView: View {
                 .foregroundColor(Color(red: 37 / 255, green: 17 / 255, blue: 97 / 255))
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 self.textSwitch.toggle()
             }
         }
